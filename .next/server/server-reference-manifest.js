16e3fca92296df8be9524a5bfb727168f343609e
Johann <fj.carl16@gmail.com>
2024-09-05 15:40:55 +0800

    Fixed .gitignore and removed previously tracked files

--- .next/server/server-reference-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nCz/+XiyMoGeAh9O/xpyIyAdHb6F8pbTStvSOypXaYA=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"I/fTeAi8ZGEv+9kKgDQir7+qXpZSruk23Hj7hax3AcY=\"\n}"
->>>>>>> 09f40122b3a477650fa430187c20c14e0e3179a8
